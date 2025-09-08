10f7eba9c9842193e31892c5a2030e9cfbddf4e3
gustavobarez <gbarez44@gmail.com>
2025-09-08 17:54:53 -0300

    chore: add Spring initial files

---
+++ src/main/java/br/com/gustavobarez/Kairo/KairoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarez.Kairo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KairoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavobarez/Kairo/KairoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarez.Kairo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
